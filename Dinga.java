53bb007d74850723f9929efb5235baba9641f9eb
adarsh-holi017 <129885946+adarsh-holi017@users.noreply.github.com>
2023-04-04 23:05:20 +0530

    Add files via upload

---
+++ Dinga.java
@@ -0,0 +1,10 @@
+class Dinga
+{
+public static void main(String[] args]
+{
+int age=21;
+double height=5.3;
+char gender='M';
+System.out.println("Dinga age is "+age+" years with height"+height+" feet and gender is"+gender);
+}
+}
